5160da976e2cb4a7c39e7778e1d689115d27aafe
yordan.ramchev <yordan.ramchev@mentormate.com>
2023-10-12 19:43:43 +0300

    Add comment that links to the documentation

--- utils/generateIndex.ts
+++ utils/generateIndex.ts
@@ -1,59 +1,59 @@
 export default function generateIndex({ needsPinia, needsRouter, needsVueAxe, needsI18n }) {
   let indexFile = `
 import './assets/main.css'
-import { createApp } from 'vue'
-`
+import { createApp } from 'vue'`
 
   if (needsPinia) {
     indexFile += `
-import { createPinia } from 'pinia'
-`
+import { createPinia } from 'pinia'`
   }
 
   if (needsRouter) {
     indexFile += `
-import router from './router'
-`
+import router from './router'`
   }
 
   if (needsI18n) {
     indexFile += `
-import { createI18n } from 'vue-i18n'
-`
+import { createI18n } from 'vue-i18n'`
   }
 
-  indexFile += `import App from './App.vue'`
+  indexFile += `
+import App from './App.vue'
+`
 
   if (needsI18n) {
     indexFile += `
-  const i18n = createI18n({
-    legacy: false, // you must set false, to use Composition API
-    locale: 'ja', // set locale
-    fallbackLocale: 'en', // set fallback locale
-    messages: {
-      en: {
-        msg: 'hello',
-        named: '{msg} world!',
-        list: '{0} world!',
-        literal: "{'hello'} world!",
-        the_world: 'the world',
-        dio: 'DIO:',
-        linked: '@:dio @:the_world !!!!'
-      },
-      ja: {
-        msg: 'こんにちは',
-        named: '{msg} 世界！',
-        list: '{0} 世界！',
-        literal: "{'こんにちは'} 世界！",
-        the_world: 'ザ・ワールド！',
-        dio: 'ディオ:',
-        linked: '@:dio @:the_world ！！！！'
-      }
+// https://vue-i18n.intlify.dev/guide/introduction.html
+const i18n = createI18n({
+  legacy: false, // you must set false, to use Composition API
+  locale: 'ja', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages: {
+    en: {
+      msg: 'hello',
+      named: '{msg} world!',
+      list: '{0} world!',
+      literal: "{'hello'} world!",
+      the_world: 'the world',
+      dio: 'DIO:',
+      linked: '@:dio @:the_world !!!!'
+    },
+    ja: {
+      msg: 'こんにちは',
+      named: '{msg} 世界！',
+      list: '{0} 世界！',
+      literal: "{'こんにちは'} 世界！",
+      the_world: 'ザ・ワールド！',
+      dio: 'ディオ:',
+      linked: '@:dio @:the_world'
     }
-  })`
+  }
+})`
   }
 
-  indexFile += 'const app = createApp(App)'
+  indexFile += `
+const app = createApp(App)`
 
   if (needsPinia) {
     indexFile += `
