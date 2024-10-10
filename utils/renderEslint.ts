504a20d37136f6568b1a542ab44757ea63822a1a
Yordan Ramchev <yordan.ramchev@mentormate.com>
2024-10-10 12:34:07 +0300

    feat: update eslint to v9 (#306)
    * feat: update vite url
    
    * feat: support ESLint 9 Flat Config
    
    * chore: releasse prep
    
    * test: readd eslint tests
    
    * chore: use single quotes as in the other configs
    
    * chore: bumps to latest esbuild, allowing to remove the type assertion hack
    
    * chore: bumps to latest esbuild, allowing to remove the type assertion hack
    
    * chore(deps): update dependency @vue/eslint-config-typescript to v14 (#304)
    
    * chore(deps): update dependency @vue/eslint-config-typescript to v14
    
    * chore(deps): update all non-major dependencies (#307)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Yordan Ramchev <yordan.ramchev@mentormate.com>
    
    * chore(deps): update dependency chromedriver to ^129.0.3 (#308)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Yordan Ramchev <yordan.ramchev@mentormate.com>
    
    * chore(deps): update dependency @vue/eslint-config-typescript to v14 (#309)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Yordan Ramchev <yordan.ramchev@mentormate.com>
    
    * chore(deps): update dependency eslint to v9 (#310)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore: bump up packages
    
    * feat: add @vitest/eslint-plugin
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- utils/renderEslint.ts
+++ utils/renderEslint.ts
@@ -1,8 +1,6 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
-import type { Linter } from 'eslint'
-
 import createESLintConfig from '@vue/create-eslint-config'
 
 import sortDependencies from './sortDependencies'
@@ -13,97 +11,126 @@ const eslintDeps = eslintTemplatePackage.devDependencies
 
 export default function renderEslint(
   rootDir,
-  { needsTypeScript, needsCypress, needsCypressCT, needsPlaywright }
+  { needsTypeScript, needsVitest, needsCypress, needsCypressCT, needsPrettier, needsPlaywright }
 ) {
-  const additionalConfig: Linter.Config = {}
-  const additionalDependencies = {}
-
-  if (needsCypress) {
-    additionalConfig.overrides = [
-      {
-        files: needsCypressCT
-          ? [
-              '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-              'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-              'cypress/support/**/*.{js,ts,jsx,tsx}'
-            ]
-          : ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
-        extends: ['plugin:cypress/recommended']
-      }
-    ]
-
-    additionalDependencies['eslint-plugin-cypress'] = eslintDeps['eslint-plugin-cypress']
-  }
-
-  if (needsPlaywright) {
-    // if the key already exists make sure to not override it, but add to it
-    if (additionalConfig.overrides) {
-      additionalConfig.overrides = [
-        ...additionalConfig.overrides,
-        {
-          files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-          extends: ['plugin:playwright/recommended']
-        }
-      ]
-    } else {
-      additionalConfig.overrides = [
-        {
-          files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-          extends: ['plugin:playwright/recommended']
-        }
-      ]
-    }
-
-    additionalDependencies['eslint-plugin-playwright'] = eslintDeps['eslint-plugin-playwright']
-  }
-
-  // add vuejs-accessibility
-  additionalConfig.plugins = ['vuejs-accessibility']
-  if (additionalConfig.extends) {
-    additionalConfig.extends = [
-      ...additionalConfig.extends,
-      'plugin:vuejs-accessibility/recommended'
-    ]
-  } else {
-    additionalConfig.extends = ['plugin:vuejs-accessibility/recommended']
-  }
-
-  additionalDependencies['eslint-plugin-vuejs-accessibility'] =
-    eslintDeps['eslint-plugin-vuejs-accessibility']
+  const additionalConfigs = getAdditionalConfigs({
+    needsVitest,
+    needsCypress,
+    needsCypressCT,
+    needsPlaywright
+  })
 
   const { pkg, files } = createESLintConfig({
-    vueVersion: '3.x',
-    // we currently don't support other style guides
     styleGuide: 'default',
     hasTypeScript: needsTypeScript,
-    needsPrettier: true,
-    additionalConfig,
-    additionalDependencies
+    needsPrettier,
+
+    additionalConfigs
   })
 
   const scripts: Record<string, string> = {
-    // Note that we reuse .gitignore here to avoid duplicating the configuration
-    lint: needsTypeScript
-      ? 'eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore'
-      : 'eslint . --ext .vue,.js,.jsx,.cjs,.mjs --fix --ignore-path .gitignore'
+    lint: 'eslint . --fix'
   }
 
-  // ESLint + Prettier
-  // Default to only format the `src/` directory to avoid too much noise, and
-  // the need for a `.prettierignore` file.
-  // Users can still append any paths they'd like to format to the command,
-  // e.g. `npm run format cypress/`.
-  scripts.format = 'prettier --write src/'
+  // Theoretically, we could add Prettier without requring ESLint.
+  // But it doesn't seem to be a good practice, so we just leave it here.
+  if (needsPrettier) {
+    // Default to only format the `src/` directory to avoid too much noise, and
+    // the need for a `.prettierignore` file.
+    // Users can still append any paths they'd like to format to the command,
+    // e.g. `npm run format cypress/`.
+    scripts.format = 'prettier --write src/'
+  }
 
   // update package.json
   const packageJsonPath = path.resolve(rootDir, 'package.json')
   const existingPkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
   const updatedPkg = sortDependencies(deepMerge(deepMerge(existingPkg, pkg), { scripts }))
-  fs.writeFileSync(packageJsonPath, JSON.stringify(updatedPkg, null, 2) + '\n', 'utf-8')
+  fs.writeFileSync(packageJsonPath, JSON.stringify(updatedPkg, null, 2) + '\n', 'utf8')
 
-  // write to .eslintrc.cjs, .prettierrc.json, etc.
+  // write to eslint.config.mjs, .prettierrc.json, .editorconfig, etc.
   for (const [fileName, content] of Object.entries(files)) {
     const fullPath = path.resolve(rootDir, fileName)
-    fs.writeFileSync(fullPath, content as string, 'utf-8')
+    fs.writeFileSync(fullPath, content as string, 'utf8')
   }
+}
+
+type ConfigItemInESLintTemplate = {
+  importer: string
+  content: string
+}
+type AdditionalConfig = {
+  devDependencies: Record<string, string>
+  beforeVuePlugin?: Array<ConfigItemInESLintTemplate>
+  afterVuePlugin?: Array<ConfigItemInESLintTemplate>
+}
+type AdditionalConfigArray = Array<AdditionalConfig>
+
+// visible for testing
+export function getAdditionalConfigs({
+  needsVitest,
+  needsCypress,
+  needsCypressCT,
+  needsPlaywright
+}) {
+  const additionalConfigs: AdditionalConfigArray = []
+
+  if (needsVitest) {
+    additionalConfigs.push({
+      devDependencies: { '@vitest/eslint-plugin': eslintDeps['@vitest/eslint-plugin'] },
+      afterVuePlugin: [
+        {
+          importer: `import pluginVitest from '@vitest/eslint-plugin'`,
+          content: `
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },`
+        }
+      ]
+    })
+  }
+
+  if (needsCypress) {
+    additionalConfigs.push({
+      devDependencies: { 'eslint-plugin-cypress': eslintDeps['eslint-plugin-cypress'] },
+      afterVuePlugin: [
+        {
+          importer: "import pluginCypress from 'eslint-plugin-cypress/flat'",
+          content: `
+  {
+    ...pluginCypress.configs.recommended,
+    files: [
+      ${[
+        ...(needsCypressCT ? ["'**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',"] : []),
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/support/**/*.{js,ts,jsx,tsx}'
+      ]
+        .map(JSON.stringify.bind(JSON))
+        .join(',\n      ')
+        .replace(/"/g, "'")} // use single quotes as in the other configs
+    ],
+  },`
+        }
+      ]
+    })
+  }
+
+  if (needsPlaywright) {
+    additionalConfigs.push({
+      devDependencies: { 'eslint-plugin-playwright': eslintDeps['eslint-plugin-playwright'] },
+      afterVuePlugin: [
+        {
+          importer: "import pluginPlaywright from 'eslint-plugin-playwright'",
+          content: `
+  {
+    ...pluginPlaywright.configs['flat/recommended'],
+    files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },`
+        }
+      ]
+    })
+  }
+
+  return additionalConfigs
 }
